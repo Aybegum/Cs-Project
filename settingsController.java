c3206b0480030fbbe49a5233b4c0f5d6e6ce6687
elifsutemirel <146671570+elifsutemirel@users.noreply.github.com>
2024-05-03 13:17:12 +0300

    Update settingsController.java

--- settingsController.java
+++ settingsController.java
@@ -1,12 +1,22 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Polygon;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
-
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+import org.w3c.dom.Text;
+import javafx.scene.Node;
 public class settingsController {
-
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
     private Rectangle arrowPart1;
 
