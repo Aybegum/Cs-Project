6c6e4c574a41f5993c3d4634ce5001b6b5b552a2
merve topuz <146668766+mervetopuz@users.noreply.github.com>
2024-05-03 11:25:16 +0300

    Update profileController.java

--- profileController.java
+++ profileController.java
@@ -1,2 +1,18 @@
-public void goToCommunityHub () {}
-public void goToSettings () {}
+public void profileControler {
+  public void goToCommunityHub(MouseEvent event) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("communityhubPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+}
+public void goToSettings() {
+        Parent root = FXMLLoader.load(getClass().getResource("settings.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+}
+}
+
+
