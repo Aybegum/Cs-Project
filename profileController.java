af1314b10f260caa52b3b1b0b6ea242d2267e8a3
merve topuz <146668766+mervetopuz@users.noreply.github.com>
2024-05-03 13:17:07 +0300

    Update profileController.java

--- profileController.java
+++ profileController.java
@@ -1,3 +1,18 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+import org.w3c.dom.Text;
+import javafx.scene.Node;
 public void profileControler {
   public void goToCommunityHub(MouseEvent event) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("communityhubPage.fxml"));
