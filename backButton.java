6adc410a2bf7a050fa3dba9944d747ba0c91d272
Aybegüm Fatma Çelebi <fatma.celebi@ug.bilkent.edu.tr>
2024-05-07 09:48:20 +0300

    I fixed some errors in the backbutton file and fixed some visual elements

--- backButton.java
+++ backButton.java
@@ -1,12 +1,30 @@
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent ;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+import org.w3c.dom.Text;
+import javafx.scene.Node;
 
 public class backButton {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    private Scene preScene;
     public void getPreviousScene(Scene preScene) {
         this.preScene = preScene;
     }
 
-
-    void clickPreviousScene (ActionEvent event) throws IOException {
+    void clickPreviousScene (MouseEvent event) throws IOException {
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         stage.setScene(preScene);
         stage.show();
