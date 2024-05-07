ccdbc55540a2d84fed970f7b80be4d50d3eb9573
Emir Said Bakan <emirsaidbakan@gmail.com>
2024-05-07 22:44:39 +0300

    Added a community class

---
+++ Community.java
@@ -0,0 +1,35 @@
+public class Community {
+
+    private int id;
+    private String name;
+    private static int currentCommunityId;
+
+    public Community(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static int getCurrentCommunityId() {
+        return currentCommunityId;
+    }
+
+    public static void setCurrentCommunityId(int currentCommunityId) {
+        Community.currentCommunityId = currentCommunityId;
+    }
+}
