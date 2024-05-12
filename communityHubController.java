bd270090d72b4bb7e56a02db2212aa8663caa304
Aybegüm Fatma Çelebi <fatma.celebi@ug.bilkent.edu.tr>
2024-05-12 22:02:04 +0300

    aaaa

--- communityHubController.java
+++ communityHubController.java
@@ -1,3 +1,4 @@
+import javafx.collections.ListChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -6,31 +7,36 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollBar;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+
 import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 import org.w3c.dom.Text;
 import javafx.scene.Node;
-public class communityHubController {
-    private FlowPane flowPane;
-        private Stage stage;
-        private Scene scene;
-        private Parent root;
-        private static int coordinateY = 122;
-        private static int playlistNoNCounter = 1;
-        private static Playlist playlistOnScreen;
-
-
+public class communityHubController implements Initializable{
+      private Stage stage;
+      private Scene scene;
+      private Parent root;
+      private static int playlistNoNCounter = 1;
+      private static int countPlay = 0;
+      private static Playlist playlistOnScreen;
+      @FXML
+      private FlowPane flowPane;
 
     public void goToCommunity(MouseEvent event) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("postPage.fxml"));
@@ -49,10 +55,24 @@ public class communityHubController {
         stage.show();
     }
 
-    public void renderPlaylistsOnSidebar(MouseEvent event) throws SQLException {
-
+      public void renderPlaylistsOnSidebar(MouseEvent event) throws SQLException {
             ArrayList<Playlist> playlists = new ArrayList<>();
-
+            Connection connection = Main.connect();
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select * from playlists where creatorid = '" + User.getCurrentUser().getId() + "' order by id desc");
+            int count;
+            if (rs.next()) {
+                  count = rs.getInt("id");
+                  for (int i = count - 1; i >= 0; i--) {
+                        playlists.add(Playlist.getPlaylistByIdAndUser(i, User.getCurrentUser()));
+                        playlistNoNCounter++;
+                  }
+            }
+            displayPlaylist();
+              
+      }
+      public void renderPlaylistsOnSidebar()throws SQLException{
+            ArrayList<Playlist> playlists = new ArrayList<>();
             Connection connection = Main.connect();
             Statement statement = connection.createStatement();
             ResultSet rs = statement.executeQuery("select * from playlists where creatorid = '" + User.getCurrentUser().getId() + "' order by id desc");
@@ -64,14 +84,36 @@ public class communityHubController {
                   }
             }
             for (Playlist playlist : playlists) {
-                  Button newPlaylist = new Button("Playlist");
-                  flowPane.getChildren().add(newPlaylist);
+                  displayPlaylist();
             }
-            Button newPlaylist = new Button("Playlist " + playlistNoNCounter);
-            newPlaylist.setLayoutX(437); // it is the coordinate of the liked songs button
-            newPlaylist.setLayoutY(coordinateY);
-            root.getChildrenUnmodifiable().add(newPlaylist);
-            playlistNoNCounter++;
-            coordinateY += 35;
       }
+
+      public void displayPlaylist()throws SQLException{
+            Button newPlaylist = new Button("Playlist");
+            newPlaylist.setFont(Font.font("Times New Roman", 16));
+            newPlaylist.setPrefSize(154,29);
+            flowPane.getChildren().add(newPlaylist);
+            flowPane.getChildren().addListener(new ListChangeListener<Node>() {
+                  @Override
+                  public void onChanged(Change<? extends Node> c) {
+                      while (c.next()) {
+                          if (c.wasAdded()) {
+                              flowPane.setPrefHeight(flowPane.getHeight() + 50);
+                          }
+                      }
+                  }
+              });
+      }
+      public void createPlaylist1 (MouseEvent event) throws SQLException{
+            Playlist.createPlaylist(User.getCurrentUser(), "Playlist", "efuhjıdfsjjd");
+            renderPlaylistsOnSidebar(event);
+        } 
+        @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            renderPlaylistsOnSidebar();
+        } catch (SQLException e) {
+            System.out.println("Error in rendering messages: SQLException");
+        }
+    }
 }
