0767cfdb638a808b84388f9e08f5f07109275a9c
Beren Irgat <beren.irgat@ug.bilkent.edu.tr>
2024-05-13 01:22:01 +0300

    Updated the search page
    -Merve

--- searchpageController.java
+++ searchpageController.java
@@ -5,27 +5,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
-import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.text.Font;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.layout.HBox;
-import javafx.scene.text.Text;
 import javafx.fxml.FXML;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Polygon;
@@ -55,9 +45,9 @@ public class searchpageController {
     private Parent root;
     private static int songCounter = 1;
 
-    public void goToCommunityHub(MouseEvent  event) throws Exception{
+    public void goToCommunityHub(MouseEvent event) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("communityhubPage.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
