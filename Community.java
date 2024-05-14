20a7935e5556cbccdcb5a9a1f821f2e447827d8f
Beren Irgat <145269680+berenirgat@users.noreply.github.com>
2024-05-15 00:28:25 +0300

    latest version of Community.java

--- Community.java
+++ Community.java
@@ -54,8 +54,12 @@ public class Community {
             ResultSet rs = statement.executeQuery("select * from community where id = '" + id + "'");
             if (rs.next()) {
                 String name = rs.getString("name");
+                statement.close();
+                connection.close();
                 return new Community(id, name);
             } else {
+                statement.close();
+                connection.close();
                 return null;
             }
         } catch (Exception e) {
