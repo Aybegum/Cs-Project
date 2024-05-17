2fc309ef1adb6afe492c627ae2c6b7b15337085a
Beren Irgat <145269680+berenirgat@users.noreply.github.com>
2024-05-17 23:45:30 +0300

    Update Post.java

--- Post.java
+++ Post.java
@@ -1,26 +1,49 @@
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Post {
-	
+
 	private int id;
 	private String text;
 	private String imageUrl;
+	private String title;
 	private int songId;
 	private int playlistId;
 	private int userId;
-	
-	public Post(int id, String text, String imageUrl, int songId, int playlistId, int userId) {
+	private int communityId;
+
+	public Post(int id, String title, String text, String imageUrl, int songId, int playlistId, int userId,
+			int communityId) {
 		this.id = id;
 		this.text = text;
 		this.imageUrl = imageUrl;
 		this.songId = songId;
 		this.playlistId = playlistId;
 		this.userId = userId;
+		this.communityId = communityId;
+		this.title = title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public int getCommunityId() {
+		return communityId;
+	}
+
+	public void setCommunityId(int communityId) {
+		this.communityId = communityId;
 	}
 
 	public int getId() {
@@ -70,91 +93,128 @@ public class Post {
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
-	
-	public static Post createTextPost(String text, int userId) throws SQLException {
-		
+
+	public static Post createTextPost(String text, int userId, int communityId, String title) throws SQLException {
+
 		Connection connection = Main.connect();
 		Statement idStatement = connection.createStatement();
-		ResultSet r = idStatement.executeQuery("select id from posts order by id desc");
-		r.next();
-		int id = r.getInt(1) + 1;
-		
-		String query = "insert into posts (id, text, userId) values (?, ?, ?)";
+		ResultSet r = idStatement.executeQuery("select postid from posts order by postid desc");
+		int id = 1;
+		if (r.next()) {
+			id = r.getInt(1) + 1;
+		}
+
+		String query = "insert into posts (postid, text, userId, communityId, title) values (?, ?, ?, ?, ?)";
 		PreparedStatement statement = connection.prepareStatement(query);
 		statement.setInt(1, id);
 		statement.setString(2, text);
 		statement.setInt(3, userId);
+		statement.setInt(4, communityId);
+		statement.setString(5, title);
 		statement.executeUpdate();
-		
+
 		statement.close();
 		connection.close();
-		
-		return new Post(id, text, null, -1, -1, userId);
-		
+
+		return new Post(id, title, text, null, -1, -1, userId, communityId);
+
 	}
-	
-	public static Post createImagePost(String imageUrl, int userId) throws SQLException{
-		
+
+	public static Post createImagePost(String imageUrl, int userId, int communityId, String title) throws SQLException {
+
 		Connection connection = Main.connect();
 		Statement idStatement = connection.createStatement();
-		ResultSet r = idStatement.executeQuery("select id from posts order by id desc");
-		r.next();
-		int id = r.getInt(1) + 1;
-		
-		String query = "insert into posts (id, imageUrl, userId) values (?, ?, ?)";
+		ResultSet r = idStatement.executeQuery("select postid from posts order by postid desc");
+		int id = 1;
+		if (r.next()) {
+			id = r.getInt(1) + 1;
+		}
+
+		String query = "insert into posts (postid, imageUrl, userId, communityId, title) values (?, ?, ?, ?, ?)";
 		PreparedStatement statement = connection.prepareStatement(query);
 		statement.setInt(1, id);
 		statement.setString(2, imageUrl);
 		statement.setInt(3, userId);
+		statement.setInt(4, communityId);
+		statement.setString(5, title);
 		statement.executeUpdate();
-		
+
 		statement.close();
 		connection.close();
-		
-		return new Post(id, imageUrl, null, -1, -1, userId);
-		
+
+		return new Post(id, title, imageUrl, null, -1, -1, userId, communityId);
+
 	}
-	
-	public static Post createSongPost(int songId, int userId) throws SQLException {
-		
+
+	public static Post createSongPost(int songId, int userId, int communityId, String title) throws SQLException {
+
 		Connection connection = Main.connect();
 		Statement idStatement = connection.createStatement();
-		ResultSet r = idStatement.executeQuery("select id from posts order by id desc");
-		r.next();
-		int id = r.getInt(1) + 1;
-		
-		String query = "insert into posts (id, songId, userId) values (?, ?, ?)";
+		ResultSet r = idStatement.executeQuery("select postid from posts order by postid desc");
+		int id = 1;
+		if (r.next()) {
+			id = r.getInt(1) + 1;
+		}
+
+		String query = "insert into posts (postid, songId, userId, communityId, title) values (?, ?, ?, ?, ?)";
 		PreparedStatement statement = connection.prepareStatement(query);
 		statement.setInt(1, id);
 		statement.setInt(2, songId);
 		statement.setInt(3, userId);
+		statement.setInt(4, communityId);
+		statement.setString(5, title);
 		statement.executeUpdate();
-		
+
 		statement.close();
 		connection.close();
-		
-		return new Post(id, null, null, songId, -1, userId);
-		
+
+		return new Post(id, title, null, null, songId, -1, userId, communityId);
+
 	}
-	
-	public static Post createPlaylistPost(int playlistId, int userId) throws SQLException{
-		
+
+	public static Post createPlaylistPost(int playlistId, int userId, int communityId, String title)
+			throws SQLException {
+
 		Connection connection = Main.connect();
 		Statement idStatement = connection.createStatement();
-		ResultSet r = idStatement.executeQuery("select id from posts order by id desc");
-		r.next();
-		int id = r.getInt(1) + 1;
-		
-		String query = "insert into posts (id, text, playlistId) values (?, ?, ?)";
+		ResultSet r = idStatement.executeQuery("select postid from posts order by postid desc");
+		int id = 1;
+		if (r.next()) {
+			id = r.getInt(1) + 1;
+		}
+
+		String query = "insert into posts (postid, playlistId, userId, communityId, title) values (?, ?, ?, ?, ?)";
 		PreparedStatement statement = connection.prepareStatement(query);
 		statement.setInt(1, id);
 		statement.setInt(2, playlistId);
 		statement.setInt(3, userId);
+		statement.setInt(4, communityId);
+		statement.setString(5, title);
 		statement.executeUpdate();
-		
+
 		statement.close();
 		connection.close();
-		
-		return new Post(id, null, null, -1, playlistId, userId);
-	}	
+
+		return new Post(id, title, null, null, -1, playlistId, userId, communityId);
+	}
+
+	public static ArrayList<Post> getPostsByCommunityId(int communityId) throws Exception {
+		ArrayList<Post> posts = new ArrayList<Post>();
+		Connection connection = Main.connect();
+		Statement statement = connection.createStatement();
+		ResultSet r = statement.executeQuery("select * from posts where communityId = '" + communityId + "'");
+		while (r.next()) {
+			int id = r.getInt("postid");
+			String title = r.getString("title");
+			String text = r.getString("text");
+			String imageUrl = r.getString("imageUrl");
+			int songId = r.getInt("songid");
+			int playlistId = r.getInt("playlistid");
+			int userId = r.getInt("userid");
+			posts.add(new Post(id, title, text, imageUrl, songId, playlistId, userId, communityId));
+		}
+		statement.close();
+		connection.close();
+		return posts;
+	}
 }
